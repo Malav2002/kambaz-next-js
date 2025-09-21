48801440e66dd3ee2ad6ed172619b783b262ead0
Malav Patel <malavpatel2002@icloud.com>
2025-09-21 13:24:17 -0400

    Update Assignments and Assignment Editor to match textbook UI

--- src/app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
+++ src/app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
@@ -6,24 +6,37 @@ export default function Assignments() {
       <input placeholder="Search for Assignments" id="wd-search-assignment" />
       <button id="wd-add-assignment-group">+ Group</button>
       <button id="wd-add-assignment">+ Assignment</button>
+      
       <h3 id="wd-assignments-title">
         ASSIGNMENTS 40% of Total <button>+</button>
       </h3>
+      
       <ul id="wd-assignment-list">
         <li className="wd-assignment-list-item">
           <Link href="/Courses/1234/Assignments/123" className="wd-assignment-link">
             A1 - ENV + HTML
           </Link>
+          <br />
+          Multiple Modules | Not available until May 6 at 12:00am |<br />
+          <strong>Due</strong> May 13 at 11:59pm | 100 pts
         </li>
+        
         <li className="wd-assignment-list-item">
           <Link href="/Courses/1234/Assignments/124" className="wd-assignment-link">
             A2 - CSS + BOOTSTRAP
           </Link>
+          <br />
+          Multiple Modules | Not available until May 13 at 12:00am |<br />
+          <strong>Due</strong> May 20 at 11:59pm | 100 pts
         </li>
+        
         <li className="wd-assignment-list-item">
           <Link href="/Courses/1234/Assignments/125" className="wd-assignment-link">
             A3 - JAVASCRIPT + REACT
           </Link>
+          <br />
+          Multiple Modules | Not available until May 20 at 12:00am |<br />
+          <strong>Due</strong> May 27 at 11:59pm | 100 pts
         </li>
       </ul>
     </div>

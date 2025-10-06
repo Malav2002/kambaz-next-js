6e9de7c06a8b92404c25caf1cb247e1baf22cd84
Malav Patel <malavpatel2002@icloud.com>
2025-10-06 17:16:15 -0400

    error solving

--- src/app/(Kambaz)/Courses/[cid]/Navigation.tsx
+++ src/app/(Kambaz)/Courses/[cid]/Navigation.tsx
@@ -1,63 +1,39 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function CourseNavigation() {
+  const pathname = usePathname();
+
+  const links = [
+    { href: "/Courses/1234/Home", label: "Home", id: "wd-course-home-link" },
+    { href: "/Courses/1234/Modules", label: "Modules", id: "wd-course-modules-link" },
+    { href: "/Courses/1234/Piazza", label: "Piazza", id: "wd-course-piazza-link" },
+    { href: "/Courses/1234/Zoom", label: "Zoom", id: "wd-course-zoom-link" },
+    { href: "/Courses/1234/Assignments", label: "Assignments", id: "wd-course-quizzes-link" },
+    { href: "/Courses/1234/Quizzes", label: "Quizzes", id: "wd-course-assignments-link" },
+    { href: "/Courses/1234/Grades", label: "Grades", id: "wd-course-grades-link" },
+    { href: "/Courses/1234/People/Table", label: "People", id: "wd-course-people-link" },
+  ];
+
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <Link
-        href="/Courses/1234/Home"
-        id="wd-course-home-link"
-        className="list-group-item active border-0"
-      >
-        Home
-      </Link>
-      <Link
-        href="/Courses/1234/Modules"
-        id="wd-course-modules-link"
-        className="list-group-item text-danger border-0"
-      >
-        Modules
-      </Link>
-      <Link
-        href="/Courses/1234/Piazza"
-        id="wd-course-piazza-link"
-        className="list-group-item text-danger border-0"
-      >
-        Piazza
-      </Link>
-      <Link
-        href="/Courses/1234/Zoom"
-        id="wd-course-zoom-link"
-        className="list-group-item text-danger border-0"
-      >
-        Zoom
-      </Link>
-      <Link
-        href="/Courses/1234/Assignments"
-        id="wd-course-quizzes-link"
-        className="list-group-item text-danger border-0"
-      >
-        Assignments
-      </Link>
-      <Link
-        href="/Courses/1234/Quizzes"
-        id="wd-course-assignments-link"
-        className="list-group-item text-danger border-0"
-      >
-        Quizzes
-      </Link>
-      <Link
-        href="/Courses/1234/Grades"
-        id="wd-course-grades-link"
-        className="list-group-item text-danger border-0"
-      >
-        Grades
-      </Link>
-      <Link
-        href="/Courses/1234/People/Table"
-        id="wd-course-people-link"
-        className="list-group-item text-danger border-0"
-      >
-        People
-      </Link>
+      {links.map((link, index) => {
+        const isActive = pathname.startsWith(link.href);
+
+        return (
+          <Link
+            key={index}
+            href={link.href}
+            id={link.id}
+            className={`list-group-item border-0 ${
+              isActive ? "active" : "text-danger"
+            }`}
+          >
+            {link.label}
+          </Link>
+        );
+      })}
     </div>
   );
 }
--- src/app/(Kambaz)/Navigation.tsx
+++ src/app/(Kambaz)/Navigation.tsx
@@ -1,13 +1,25 @@
 "use client";
-
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { ListGroup } from "react-bootstrap";
 import { AiOutlineDashboard } from "react-icons/ai";
 import { BiBook, BiCalendar } from "react-icons/bi";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { HiMiniInboxArrowDown } from "react-icons/hi2";
 import { IoIosCodeWorking } from "react-icons/io";
+
 export default function KambazNavigation() {
+  const pathname = usePathname();
+
+  const links = [
+    { href: "/Account", icon: FaRegCircleUser, label: "Account", id: "wd-account-link" },
+    { href: "/Dashboard", icon: AiOutlineDashboard, label: "Dashboard", id: "wd-dashboard-link" },
+    { href: "/Dashboard", icon: BiBook, label: "Courses", id: "wd-course-link" },
+    { href: "/Calendar", icon: BiCalendar, label: "Calendar", id: "wd-calendar-link" },
+    { href: "/Inbox", icon: HiMiniInboxArrowDown, label: "Inbox", id: "wd-inbox-link" },
+    { href: "/Labs", icon: IoIosCodeWorking, label: "Labs", id: "wd-labs-link" },
+  ];
+
   return (
     <ListGroup
       className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2"
@@ -23,72 +35,34 @@ export default function KambazNavigation() {
       >
         <img src="/images/NEU.png" width="75px" alt="Northeastern University" />
       </ListGroup.Item>
-      <ListGroup.Item className="border-0 bg-black text-center">
-        <Link
-          href="/Account"
-          id="wd-account-link"
-          className="text-white text-decoration-none"
-        >
-          <FaRegCircleUser className="fs-1 text-white" />
-          <br />
-          Account
-        </Link>
-      </ListGroup.Item>
-      <ListGroup.Item className="border-0 bg-white text-center">
-        <Link
-          href="/Dashboard"
-          id="wd-dashboard-link"
-          className="text-danger text-decoration-none"
-        >
-          <AiOutlineDashboard className="fs-1 text-danger" />
-          <br />
-          Dashboard
-        </Link>
-      </ListGroup.Item>
-      <ListGroup.Item className="border-0 bg-black text-center">
-        <Link
-          href="/Dashboard"
-          id="wd-course-link"
-          className="text-danger text-decoration-none"
-        >
-          <BiBook className="fs-1 text-danger" />
-          <br />
-          <span className="text-white">Course</span>
-        </Link>
-      </ListGroup.Item>
-      <ListGroup.Item className="border-0 bg-black text-center">
-        <Link
-          href="/Calendar"
-          id="wd-calendar-link"
-          className="text-danger text-decoration-none"
-        >
-          <BiCalendar className="fs-1 text-danger" />
-          <br />
-          <span className="text-white">Calendar</span>
-        </Link>
-      </ListGroup.Item>
-      <ListGroup.Item className="border-0 bg-black text-center">
-        <Link
-          href="/Inbox"
-          id="wd-inbox-link"
-          className="text-danger text-decoration-none"
-        >
-          <HiMiniInboxArrowDown className="fs-1 text-danger" />
-          <br />
-          <span className="text-white">Inbox</span>
-        </Link>
-      </ListGroup.Item>
-      <ListGroup.Item className="border-0 bg-black text-center">
-        <Link
-          href="/Labs"
-          id="wd-labs-link"
-          className="text-danger text-decoration-none"
-        >
-          <IoIosCodeWorking className="fs-1 text-danger" />
-          <br />
-          <span className="text-white">Labs</span>
-        </Link>
-      </ListGroup.Item>
+
+      {links.map((link, index) => {
+        const Icon = link.icon;
+        const isActive = pathname.startsWith(link.href);
+
+        return (
+          <ListGroup.Item
+            key={index}
+            className={`border-0 text-center ${
+              isActive ? "bg-white" : "bg-black"
+            }`}
+          >
+            <Link
+              href={link.href}
+              id={link.id}
+              className="text-decoration-none d-block"
+            >
+              <Icon
+                className={`fs-1 ${isActive ? "text-danger" : "text-white"}`}
+              />
+              <br />
+              <span className={isActive ? "text-danger" : "text-white"}>
+                {link.label}
+              </span>
+            </Link>
+          </ListGroup.Item>
+        );
+      })}
     </ListGroup>
   );
 }

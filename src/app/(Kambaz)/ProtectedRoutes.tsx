565fdc7118cd6f725e419d7150573ce2ec6b0df6
Malav Patel <malavpatel2002@icloud.com>
2025-11-17 17:06:28 -0500

    roll backl

--- src/app/(Kambaz)/ProtectedRoutes.tsx
+++ src/app/(Kambaz)/ProtectedRoutes.tsx
@@ -1,13 +1,5 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-type RootState = {
-  accountReducer: {
-    currentUser: {
-      role: string;
-    } | null;
-  };
-};
-
 export default function ProtectedRoute({ children }: { children: any }) {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const isAdminOrFaculty = currentUser.role === "FACULTY" || currentUser.role === "ADMIN";
